500680068aa7b3df04d61ed301892d299228b1ef
ayaanmd100 <109349651+ayaanmd100@users.noreply.github.com>
2022-09-24 19:24:48 +0530

    ver 0.1

---
+++ Anti-Idle.lua
@@ -0,0 +1,6 @@
+game.Players.LocalPlayer.Idled:Connect(function()
+			local VirtualUser = game:GetService("VirtualUser")
+			VirtualUser:CaptureController()
+			VirtualUser:ClickButton2(Vector2.new())
+end)
+
